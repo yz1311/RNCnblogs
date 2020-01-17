1de30ae8d04ccfc307426090104fa1788e481e40
yangzhao <yz1311@gmail.com>
2020-01-17 16:52:13 +0800

    修复闪存评论的头像显示

--- src/pages/status/status_detail.tsx
+++ src/pages/status/status_detail.tsx
@@ -44,6 +44,7 @@ import {ServiceTypes} from "../YZTabBarView";
 import {Api} from "../../api";
 import {statusCommentModel, statusModel} from "../../api/status";
 import {createReducerResult, dataToReducerResult, ReducerResult} from "../../utils/requestUtils";
+import {blogCommentModel} from "../../api/blog";
 
 interface IProps extends IBaseDataPageProps {
   clearStatusCommentListFn?: any;
@@ -135,10 +136,43 @@ export default class status_detail extends PureComponent<IProps, IState> {
         commentList: imgRes.data,
         getCommentListResult: dataToReducerResult(imgRes.data)
       });
+      //获取头像
+      this.getUserAvatar();
     } catch (e) {
       this.setState({
         getCommentListResult: dataToReducerResult(e)
       })
+    }
+  }
+
+  getUserAvatar = async ()=>{
+    for (let index in this.state.commentList) {
+      let item = this.state.commentList[index];
+      if(!item.author?.avatar || item.author?.avatar=='') {
+        try {
+          let imgRes = await Api.profile.getUserAvatar({
+            request: {
+              userId: (item as statusCommentModel).author?.id
+            }
+          });
+          let nextDateList = [
+            ...this.state.commentList.slice(0,parseInt(index)),
+            {
+              ...item,
+              author: {
+                ...item.author,
+                avatar: imgRes.data.avatar
+              }
+            },
+            ...this.state.commentList.slice(parseInt(index)+1),
+          ];
+          this.setState({
+            commentList: nextDateList
+          })
+        } catch (e) {
+
+        }
+      }
     }
   }
 
